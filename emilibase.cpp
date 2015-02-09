1a6beb07d3155831374e5eedf5dc5016cf75cc9a
Federico Pagnozzi <federico.pagnozzi@ulb.ac.be>
2015-02-09 09:25:12 +0100

    grammar modified so that should be easier for irace to pick ils

--- emilibase.cpp
+++ emilibase.cpp
@@ -92,6 +92,7 @@ static void finalise (int _)
        //std::cout << "Reached at time: " << (s_time - beginTime) / (float)CLOCKS_PER_SEC << std::endl;
         //std::cerr << (endTime - beginTime) / (float)CLOCKS_PER_SEC << " ";
         std::cerr << s_cap->getSolutionValue() << std::endl;
+
         std::cerr << std::flush;
     }
     else
