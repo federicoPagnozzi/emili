2e47729ecea702ddef76e8b20018660e85236eb7
Federico Pagnozzi <federico.pagnozzi@ulb.ac.be>
2015-08-28 14:31:18 +0200

    fixes to natxinsert

--- permutationflowshop.h
+++ permutationflowshop.h
@@ -654,19 +654,10 @@ public:
 class NatxNeighborhood: public emili::pfsp::HeavilyApproximatedTaillardAcceleratedInsertNeighborhood
 {
 protected:
-    //std::vector < int > apjw;
+    float aptre;
     virtual Solution* computeStep(Solution *value);
 public:
-    NatxNeighborhood(PermutationFlowShop& problem):emili::pfsp::HeavilyApproximatedTaillardAcceleratedInsertNeighborhood(problem)//,apjw(njobs+1,0)
-    {
-      /*  for(int j=1; j <= njobs ; j++)
-        {
-            for(int m=1;m<=nmac;m++)
-            {
-                apjw[j] += pmatrix[j][m];
-            }
-        }*/
-    }
+    NatxNeighborhood(PermutationFlowShop& problem):emili::pfsp::HeavilyApproximatedTaillardAcceleratedInsertNeighborhood(problem),aptre(0.5*problem.getNjobs()) { }
 };
 
 class EatxNeighborhood: public emili::pfsp::HeavilyApproximatedTaillardAcceleratedInsertNeighborhood
