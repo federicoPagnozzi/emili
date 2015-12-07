9c121428d1ee3a6032bd8fa781501b0bfc05783a
Federico Pagnozzi <federico.pagnozzi@ulb.ac.be>
2015-12-07 10:41:39 +0100

    moved pfsp code to pfsp dir

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include <algorithm>
 #include "generalParser.h"
-#include "paramsparser.h"
+#include "pfsp/paramsparser.h"
 #include "setup.h"
 
 
@@ -14,6 +14,7 @@ void g2c_info()
     std::cout << "usage in grammar2code mode : \n\tEMILI instance_file_path time random_seed" << std::endl;
     exit(0);
 }
+
 
 int main(int argc, char *argv[])
 {
