16fb3ad793a122c1c24f6ce4c83a9a0e5aa80133
Alberto Franzin <alberto.franzin@gmail.com>
2017-04-12 12:08:38 +0200

    f.ng generalparser

--- generalParser.cpp
+++ generalParser.cpp
@@ -20,7 +20,7 @@
 #define DEFAULT_TS 10
 #define DEFAULT_TI 10
 #define DEFAULT_IT 0
-#define GIT_COMMIT_NUMBER "6235cac66cb48b0e5df7b1222a1780684d720732"
+#define GIT_COMMIT_NUMBER "0b1333c1bc051b02233d60f5e696c924a257b8bf"
 /*Base Algos */
 #define IG "ig"
 #define ILS "ils"
